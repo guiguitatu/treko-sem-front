ad9791b21761a90e1d4338a61262a4d2eaf193fb
enzo <e.enrico2005@gmail.com>
2025-04-07 17:43:31 -0300

    lolll

--- src/app/modules/auth/login/page.tsx
+++ src/app/modules/auth/login/page.tsx
@@ -66,7 +66,7 @@ export default function LoginPage() {
           </form>
           <p className="mt-4 text-center text-sm text-gray-600">
             Não tem uma conta?{' '}
-            <a href="#" className="text-blue-600 hover:underline">
+            <a href="/modules/auth/register" className="text-blue-600 hover:underline">
               Cadastre-se
             </a>
           </p>
--- src/app/modules/auth/register/page.tsx
+++ src/app/modules/auth/register/page.tsx
@@ -51,7 +51,7 @@ export default function LoginPage() {
           </form>
           <p className="mt-4 text-center text-sm text-gray-600">
             Não tem uma conta?{' '}
-            <a href="#" className="text-blue-600 hover:underline">
+            <a href="/modules/base/dashboard" className="text-blue-600 hover:underline">
               Cadastre-se
             </a>
           </p>
